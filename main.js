6f72abb66028a4071697de5ef5398dea6ef0475d
TotallyMoxie <134233554+TotallyMoxie@users.noreply.github.com>
2023-06-27 21:12:24 -0500

    some js added

---
+++ main.js
@@ -0,0 +1,7 @@
+const adj1 = document.getElementById("adj1");
+const noun1 = document.getElementById("noun1");
+const adj2 = document.getElementById("adj2");
+const clothing = document.getElementById("clothing");
+const bodyPart = document.getElementById("bodyPart");
+const verb1 = document.getElementById("verb1");
+const food = document.getElementById("food");
